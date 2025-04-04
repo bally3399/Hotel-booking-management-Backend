e644cc4aacfbdd8c7899bd09bec7c17563cd2290
Baliqis <sulaimabaliqis@gmail.com>
2025-04-04 17:35:06 +0100

    finished test cases and admin controller

--- src/main/java/topg/bimber_user_service/controller/AdminController.java
+++ src/main/java/topg/bimber_user_service/controller/AdminController.java
@@ -1,10 +1,8 @@
 package topg.bimber_user_service.controller;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import topg.bimber_user_service.dto.requests.*;
 import topg.bimber_user_service.dto.responses.*;
