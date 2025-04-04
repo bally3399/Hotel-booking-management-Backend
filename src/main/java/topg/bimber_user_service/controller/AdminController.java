3193a161407c1e569dbb1d36971525ab2095a4b0
Baliqis <sulaimabaliqis@gmail.com>
2025-04-04 17:30:58 +0100

    finished test cases and admin controller

--- src/main/java/topg/bimber_user_service/controller/AdminController.java
+++ src/main/java/topg/bimber_user_service/controller/AdminController.java
@@ -6,9 +6,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import topg.bimber_user_service.dto.requests.UserAndAdminUpdateDto;
-import topg.bimber_user_service.dto.responses.UserResponseDto;
+import topg.bimber_user_service.dto.requests.*;
+import topg.bimber_user_service.dto.responses.*;
+import topg.bimber_user_service.models.State;
 import topg.bimber_user_service.service.AdminServiceImpl;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -20,37 +24,130 @@ public class AdminController {
 
     private final AdminServiceImpl adminServiceImpl;
 
-
-    @GetMapping("/me/{id}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ResponseEntity<UserResponseDto> getAdminById(@PathVariable("id") String userId) {
-        UserResponseDto message = adminServiceImpl.getAdminById(userId);
-        return ResponseEntity.ok(message);
+    @PutMapping("/update")
+    public ResponseEntity<UpdateDetailsResponse> updateAdmin(@RequestBody UpdateDetailsRequest updateUserRequest) {
+        UpdateDetailsResponse response = adminServiceImpl.updateAdmin(updateUserRequest);
+        return ResponseEntity.ok(response);
     }
 
-//    @PutMapping("/me/edit/{id}")
-//    @PreAuthorize("hasRole('ROLE_ADMIN')")
-//    public ResponseEntity<UserResponseDto> editUserById(@Valid @RequestBody UserAndAdminUpdateDto adminUpdateRequestDto, @PathVariable("id") String userId) {
-//        UserResponseDto message = adminServiceImpl.editAdminById(adminUpdateRequestDto, userId);
-//        return ResponseEntity.ok(message);
-//    }
-//
-//    @DeleteMapping("/me/delete/{id}")
-//    @PreAuthorize("hasRole('ROLE_ADMIN')")
-//    public ResponseEntity<String> deleteAdminById(@PathVariable("id") String userId) {
-//        String message = adminServiceImpl.deleteAdminById(userId);
-//        return ResponseEntity.ok(message);
-//    }
-//
-//    @GetMapping("accountVerification/{token}")
-//    public ResponseEntity<String> verifyAccount(@PathVariable String token) {
-//        try {
-//            adminServiceImpl.verifyToken(token);  // Assuming verifyToken checks the token validity and does the necessary action
-//            return new ResponseEntity<>("Account created successfully", OK);
-//        } catch (Exception e) {
-//            // Handle errors like invalid token
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid or expired token.");
-//        }
-//    }
+    @PostMapping("/rooms")
+    public ResponseEntity<RoomResponse> addRoom(@RequestBody RoomRequest roomRequest,
+                                                @RequestParam("files") List<String> multipartFiles) {
+        RoomResponse response = adminServiceImpl.addRoom(roomRequest, multipartFiles);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/hotels")
+    public ResponseEntity<HotelResponseDto> createHotel(@RequestBody CreateHotelDto createHotelDto) {
+        HotelResponseDto response = adminServiceImpl.createHotel(createHotelDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/hotels/state/{state}")
+    public ResponseEntity<List<HotelDtoFilter>> getHotelsByState(@PathVariable State state) {
+        List<HotelDtoFilter> hotels = adminServiceImpl.getHotelsByState(state);
+        return ResponseEntity.ok(hotels);
+    }
+
+    @PutMapping("/hotels/{id}")
+    public ResponseEntity<HotelResponseDto> editHotelById(@PathVariable Long id,
+                                                          @RequestBody HotelRequestDto updatedHotelDto) {
+        HotelResponseDto response = adminServiceImpl.editHotelById(id, updatedHotelDto);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/hotels/{id}")
+    public ResponseEntity<HotelDtoFilter> getHotelById(@PathVariable Long id) {
+        HotelDtoFilter response = adminServiceImpl.getHotelById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/hotels/{id}")
+    public ResponseEntity<String> deleteHotelById(@PathVariable Long id) {
+        String response = adminServiceImpl.deleteHotelById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/hotels/most-booked/{state}")
+    public ResponseEntity<List<HotelDtoFilter>> getMostBookedHotelInState(@PathVariable State state) {
+        List<HotelDtoFilter> hotels = adminServiceImpl.getMostBookedHotelInState(state);
+        return ResponseEntity.ok(hotels);
+    }
+
+    @GetMapping("/hotels/in-state/{state}")
+    public ResponseEntity<List<HotelDtoFilter>> getHotelsInState(@PathVariable State state) {
+        List<HotelDtoFilter> hotels = adminServiceImpl.getHotelsInState(state);
+        return ResponseEntity.ok(hotels);
+    }
+
+    @GetMapping("/filter/price-and-state")
+    public ResponseEntity<List<RoomResponse>> filterByPriceAndState(
+            @RequestParam("minPrice") BigDecimal minPrice,
+            @RequestParam("maxPrice") BigDecimal maxPrice,
+            @RequestParam("state") State state) {
+        List<RoomResponse> rooms = adminServiceImpl.filterByPriceAndState(minPrice, maxPrice, state);
+        return ResponseEntity.ok(rooms);
+    }
+
+    @GetMapping("/hotel/{hotelId}/type/{type}")
+    public ResponseEntity<List<RoomResponse>> filterHotelRoomByType(
+            @PathVariable("hotelId") long hotelId,
+            @PathVariable("type") String type) {
+        List<RoomResponse> rooms = adminServiceImpl.filterHotelRoomByType(hotelId, type);
+        return ResponseEntity.ok(rooms);
+    }
+
+    @PatchMapping("/hotel/{hotelId}/deactivate/{roomId}")
+    public ResponseEntity<RoomResponse> deactivateRoomByHotelId(
+            @PathVariable("hotelId") long hotelId,
+            @PathVariable("roomId") long roomId) {
+        RoomResponse response = adminServiceImpl.deactivateRoomByHotelId(hotelId, roomId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/hotel/{hotelId}/activate/{roomId}")
+    public ResponseEntity<RoomResponse> activateRoomByHotelId(
+            @PathVariable("hotelId") long hotelId,
+            @PathVariable("roomId") long roomId) {
+        RoomResponse response = adminServiceImpl.activateRoomByHotelId(hotelId, roomId);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/hotel/{hotelId}/available")
+    public ResponseEntity<List<RoomResponse>> findAllAvailableHotelRooms(
+            @PathVariable("hotelId") long hotelId) {
+        List<RoomResponse> rooms = adminServiceImpl.findAllAvailableHotelRooms(hotelId);
+        return ResponseEntity.ok(rooms);
+    }
+
+    @GetMapping("/{roomId}/availability")
+    public ResponseEntity<Boolean> isRoomAvailable(
+            @PathVariable("roomId") long roomId) {
+        boolean isAvailable = adminServiceImpl.isRoomAvailable(roomId);
+        return ResponseEntity.ok(isAvailable);
+    }
+
+    @GetMapping("/hotel/{hotelId}")
+    public ResponseEntity<List<RoomResponse>> findAllRoomsByHotelId(
+            @PathVariable("hotelId") long hotelId) {
+        List<RoomResponse> rooms = adminServiceImpl.findAllRoomsByHotelId(hotelId);
+        return ResponseEntity.ok(rooms);
+    }
+
+    @DeleteMapping("/{roomId}")
+    public ResponseEntity<String> deleteRoomById(
+            @PathVariable("roomId") long roomId) {
+        String response = adminServiceImpl.deleteRoomById(roomId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{roomId}")
+    public ResponseEntity<String> editRoomById(
+            @PathVariable("roomId") long roomId,
+            @RequestBody RoomRequest roomRequest) {
+        String response = adminServiceImpl.editRoomById(roomId, roomRequest);
+        return ResponseEntity.ok(response);
+    }
+
 
 }
