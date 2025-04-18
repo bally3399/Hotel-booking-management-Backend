912e2c1b9dae119e04d9b61691a6af439f9c5ad2
philip <ajibolaphilip10@gmail.com>
2025-04-18 15:31:55 +0100

    commit

--- src/main/java/topg/bimber_user_service/dto/requests/BookingRequestDto.java
+++ src/main/java/topg/bimber_user_service/dto/requests/BookingRequestDto.java
@@ -1,5 +1,9 @@
 package topg.bimber_user_service.dto.requests;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,7 +20,11 @@ public class BookingRequestDto {
     private String userId;
     private Long roomId;
     private Long hotelId;
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using= LocalDateTimeSerializer.class)
     private LocalDateTime startDate;
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using= LocalDateTimeSerializer.class)
     private LocalDateTime endDate;
     private Boolean isPaid;
 
